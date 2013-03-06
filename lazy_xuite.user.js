bb08d6fb5d3151195d30955365e155975ad473d4
Karl Li <killtw@gmail.com>
2013-03-06 11:13:43 +0800

    因應 xuite 更新修正 code

--- lazy_xuite.user.js
+++ lazy_xuite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             lazy_xuite
 // @name           Lazy Xuite
-// @version        1.0.5
+// @version        1.0.6
 // @namespace      http://blog.k2ds.net/
 // @author         killtw
 // @description    方便下載Xuite檔案，跳過必須按廣告的限制，並且取消倒數
@@ -12,11 +12,13 @@
 (function() {
   var req = new XMLHttpRequest();
   if (location.search != "") {
-    location.assign('javascript: var time = 10;');
+    location.assign('javascript: $("#global").data("time", 10);');
   } else {
-    var src = document.getElementById('share-download-ad').getElementsByTagName('a')[0].getAttribute('href');
-    req.open("GET", src, true);
-    req.send();
+    var src = document.getElementById('share-download-ad').getElementsByTagName('a')[0];
+    if (src != undefined) {
+      req.open("GET", src.getAttribute('href'), true);
+      req.send();
+    }
     document.getElementById('verify_code_value').onkeyup = function() {
       if (this.value.length == 6) {
         document.getElementById('save_action').value = 0;
